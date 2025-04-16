8b1ee21dd944ee223723451fb695d620ef752a78
Mateusz <mlodyajdza+github@gmail.com>
2025-04-17 00:46:31 +0200

    change config schema to singular only

--- src/platform.ts
+++ src/platform.ts
@@ -37,41 +37,48 @@ export class ShellyDoorPlatformPlugin implements DynamicPlatformPlugin {
 
 
   discoverDevices() {
-    const devices: DeviceConfig[] = this.config.devices;
+    const device: DeviceConfig = {
+      id: this.config.id,
+      name: this.config.name,
+      hostname: this.config.hostname,
+      openTime: this.config.openTime,
+      closeTime: this.config.closeTime,
+      obstructionDetection: this.config.obstructionDetection,
+    } as DeviceConfig;
 
-    this.log.debug('Devices to discovery', devices);
-    for (const device of devices) {
+    this.log.debug('Devices to discovery', device);
+    // for (const device of devices) {
 
-      const uuid = this.api.hap.uuid.generate(device.id);
+    const uuid = this.api.hap.uuid.generate(device.id);
 
-      const existingAccessory = this.accessories.get(uuid);
+    const existingAccessory = this.accessories.get(uuid);
 
-      if (existingAccessory) {
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+    if (existingAccessory) {
+      this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+      // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
+      // existingAccessory.context.device = device;
+      // this.api.updatePlatformAccessories([existingAccessory]);
 
-        new DrivewayGateAccessory(this, existingAccessory, device, this.log);
-      } else {
-        this.log.info('Adding new accessory:', device.name);
+      new DrivewayGateAccessory(this, existingAccessory, device, this.log);
+    } else {
+      this.log.info('Adding new accessory:', device.name);
 
-        const accessory = new this.api.platformAccessory(device.name, uuid);
+      const accessory = new this.api.platformAccessory(device.name, uuid);
 
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
+      // store a copy of the device object in the `accessory.context`
+      // the `context` property can be used to store any data about the accessory you may need
+      accessory.context.device = device;
 
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new DrivewayGateAccessory(this, accessory, device, this.log);
+      // create the accessory handler for the newly create accessory
+      // this is imported from `platformAccessory.ts`
+      new DrivewayGateAccessory(this, accessory, device, this.log);
 
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
-
-      this.discoveredCacheUUIDs.push(uuid);
+      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
     }
+
+    this.discoveredCacheUUIDs.push(uuid);
+    // }
 
     for (const [uuid, accessory] of this.accessories) {
       if (!this.discoveredCacheUUIDs.includes(uuid)) {
